dc4dcf041f579d9cb6ef2284e44464938cb3f698
Mike Taylor <mike.charles.taylor@gmail.com>
2024-01-17 17:10:33 -0700

    Update transmission image

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,5 @@
-FROM debian:bullseye-slim
-ENV DEBIAN_FRONTEND noninteractive
-RUN apt-get update \
-  && apt-get upgrade -y \
-  && apt-get install -y transmission-daemon \
-  && apt-get clean \
-  && rm -rf /var/lib/apt/lists/* \
+FROM docker.io/alpine:latest
+RUN apk add --no-cache transmission-daemon \
   && mkdir -p /config \
   && mkdir -p /downloads
 ADD run-transmission.sh /usr/local/bin/run-transmission.sh
--- run-transmission.sh
+++ run-transmission.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/ash
 set -euo pipefail
-ARGS=()
-ARGS+=(--foreground)
-ARGS+=(--allowed "*.*.*.*")
-ARGS+=(--config-dir ${CONFIG_DIR:-/config})
-ARGS+=(--port ${WEBUI_PORT:-9091})
-ARGS+=(--peerport ${PEER_CONNECTION_PORT:-51413})
-ARGS+=(--download-dir ${DOWNLOAD_DIR:-/downloads})
-exec transmission-daemon ${ARGS[@]}
+exec transmission-daemon \
+  --foreground \
+  --username transmission \
+  --password transmission \
+  --config-dir /config \
+  --port 9091 \
+  --peerport 51413 \
+  --download-dir /downloads
